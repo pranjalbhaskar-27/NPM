56376b997abbcd9ac15c7a068c6c5e451dd0462b
Pranjal BHaskar <bhaskarpranjal@gmail.com>
2022-03-04 20:26:22 +0530

    init added

--- npm init/main.js
+++ npm init/main.js
@@ -3,9 +3,8 @@ const sub=require('./subtract')
 const div=require('./divide')
 const mul=require('./multiply')
 
-
-
 console.log(add(1,2))
 console.log(sub(5,3))
 console.log(div(8,4))
 console.log(mul(4,3))
+
